97b0a4bb4936e21c43599e0ea716a9111c3aa2e9
phanquangdev <phanquangdev>
2022-07-12 10:21:08 +0700

    add file static and sass

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ const port = 3000;
 
 app.use(morgan('combined'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine(
   'hbs',
   handlebars.engine({
     extname: '.hbs',
   })
 );
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
